afb40182b5ecff99505482800314ddd4a1cc3447
Ozodbek_dev <99534081+ShukurovOzodbek@users.noreply.github.com>
2022-02-26 07:12:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,10 @@ let filt = newKey.filter(item => {
     }
 })
 console.log(number, string, bool);
+
+
+// 305
+console.log('This is 305')
+let arr = [[{a:{price:20}}],[{a:{price:35}}],[{a:{price:44}}]]
+let total = arr[0][0].a.price + arr[1][0].a.price + arr[2][0].a.price
+console.log(total); 
